96a4ec69033f60762427c72429d71f0bf199369b
pisache <hobulhojunlee20@gmail.com>
2025-01-25 18:28:51 +0900

    Task done

---
+++ scanPDF/main.py
@@ -0,0 +1,12 @@
+import sys
+
+from scanPDF.scanPDF import scan
+
+def main():
+    print("MAIN")
+
+    dir = sys.argv[1]
+    scan(dir)
+
+if __name__ == "__main__":
+    main()
---
+++ testpath.py
@@ -0,0 +1,3 @@
+import os
+full_output_path = r"C:\dev\harrys-tools\output.xlsx"
+print(os.path.exists(full_output_path))  # Should be True
